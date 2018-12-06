3edac7e98c813f3b137c90fc0907fc7087d1610b
sunriselink <cfljamtd@gmail.com>
2018-12-07 00:28:22 +0500

    Init project

---
+++ src/backend/EZChat.Master/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace EZChat.Master
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = WebHost.CreateDefaultBuilder(args)
+                              .UseStartup<Startup>()
+                              .Build();
+
+            host.Run();
+        }
+    }
+}
