7a45bd0959cb0cf81ed697b4b78c2159b7520559
Dmitry <dima6200@gmail.com>
2018-12-10 01:09:15 +0500

    Client update
    Create test vue messange accept

--- src/client/src/main/index.js
+++ src/client/src/main/index.js
@@ -1,27 +1,30 @@
-import {app, BrowserWindow} from 'electron';
-import path from 'path';
-import {format as formatUrl} from 'url';
+import { app, BrowserWindow } from 'electron'
+import path from 'path'
+import { format as formatUrl } from 'url'
 
-const isDevelopment = process.env.NODE_ENV !== 
-'production'
+const isDevelopment = process.env.NODE_ENV !== 'production'
 
 app.on('ready', () => {
-    let mainWindow = new BrowserWindow({
-        width: 1024
-    });
-    if(isDevelopment){
-        mainWindow.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`)
-    }else{
-        mainWindow.loadURL(formatUrl({
-            pathname: path.join(__dirname, 'index.html'),
-            protocol: 'file',
-            slashes: true
-        }))
-    }
+  let window = new BrowserWindow({
+    width: 1024,
+    backgroundColor: '#31394c'
+  })
+  if (isDevelopment) {
+    window.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`)
+  } else {
+    window.loadURL(formatUrl({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file',
+      slashes: true
+    }))
+  }
+  window.on("closed", () => {
+    window = null;
+  })
 })
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 })
---
+++ src/client/src/renderer/mainWindow.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <div class="message" v-for="message in messages" :key="message.id">
+            {{ message.content }}
+        </div>
+        <input class="messageInput" type="text" placeholder="Начните воодить сообщение..." v-model="newMessage" @keyup.enter="sendMessage">
+    </div>
+</template>
+
+
+<script>
+    export default {
+        data() {
+            return {
+                newMessage: '',
+                idForMessage: 3,
+                messages: [
+                    {
+                        'id': 1,
+                        'content': 'fist message'
+                    },
+                    {
+                        'id': 2,
+                        'content': 'second message'
+                    }
+                ]
+            }
+        },
+        methods: {
+            sendMessage() {
+                this.messages.push({
+                    id: this.idForMessage,
+                    content: this.newMessage
+                })
+                this.newMessage = ''
+                this.idForMessage++
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+@import '/scss/style.scss'
+</style>
+
